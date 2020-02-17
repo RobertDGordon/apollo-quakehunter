fe05ef054df9b04f2f26aa48c16653401a9a7bfe
Robert Gordon <Komeuppance@gmail.com>
2020-02-16 20:24:00 -0800

    Added authenticaton

--- server/index.js
+++ server/index.js
@@ -1,13 +1,44 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./schema.js');
-const resolvers = require('./resolvers')
+const resolvers = require('./resolvers');
+
+const { createStore } = require('./utils.js')
+
 const QuakeAPI = require('./datasources/quake.js');
+const UserAPI = require('./datasources/user.js');
+
+const store = createStore();
 
 const server = new ApolloServer({
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        // if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const usercheck = await store.users.map(user => {
+            if (email === user.email) {
+                return user
+            }
+        });
+
+        let users = []
+
+        await usercheck.forEach(e => {
+            if (e) {
+                users.push(e)
+            }
+        })
+
+        const user = users && users[0] || null;
+    
+        return { user };
+      },
     typeDefs,
     resolvers,
     dataSources: () => ({
-        quakeAPI: new QuakeAPI()
+        quakeAPI: new QuakeAPI(),
+        userAPI: new UserAPI({ store })
     })
 });
 
